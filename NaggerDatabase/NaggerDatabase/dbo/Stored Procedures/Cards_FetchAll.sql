8b4c81b535be413fd3ab75cf441b56af04fc71f2
GailBowen <gailbowen1@gmail.com>
2016-01-17 10:35:38 +0000

    Update database

--- NaggerDatabase/NaggerDatabase/dbo/Stored Procedures/Cards_FetchAll.sql
+++ NaggerDatabase/NaggerDatabase/dbo/Stored Procedures/Cards_FetchAll.sql
@@ -6,22 +6,22 @@ BEGIN
 	
 	SET NOCOUNT ON;
 
-	
-	SELECT 
-		t1.done as lastdone,
-		s1.done as lastskipped,
+	select 
+		done.lastDone,
+		skip.lastSkip,
 		c.*
-	FROM cards c
-	   left outer JOIN tadas t1
-	  ON (c.id = t1.cardid)
-	 LEFT OUTER JOIN tadas t2
-		ON (c.id = t2.cardid AND t1.id < t2.id)
-	  left outer JOIN skips s1
-	  ON (c.id = s1.cardid)
-	 LEFT OUTER JOIN tadas s2
-		ON (c.id = s2.cardid AND s1.id < s2.id)
-	 where t2.id is null and s2.id is null
-	      and
-		  Archived = 0
+	from
+	cards c
+	left join 
+	(
+		select cardid, max(LogDate) as lastDone from logs where type = 'DONE'  group by cardid
+	) as done
+	on done.CardID = c.id
+	left join
+	(
+		select cardid, max(logdate) as lastSkip from logs where type = 'SKIP' group by cardid
+	) as skip
+	on skip.cardid = c.id
+
 
 END
