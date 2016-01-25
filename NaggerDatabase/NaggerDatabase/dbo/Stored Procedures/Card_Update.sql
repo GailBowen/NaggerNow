38455c557a36421f82777d961243d9e32a26072e
GailBowen <gailbowen1@gmail.com>
2016-01-25 20:35:33 +0000

    Update the database project

---
+++ NaggerDatabase/NaggerDatabase/dbo/Stored Procedures/Card_Update.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [dbo].[Card_Update]
+	@id int,
+	@columnID INT,
+	@description varchar(max)
+AS
+BEGIN
+	
+	update 
+		dbo.Cards 
+	set 
+		ColumnID = @columnID,
+		Description = @description
+	where 
+		id = @id
+
+END
