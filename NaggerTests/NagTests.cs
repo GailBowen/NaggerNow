5097cdc9bb83272f7dc962018fbb32d83e860edb
GailBowen <gailbowen1@gmail.com>
2016-01-26 22:01:34 +0000

    Make a start on tests.

---
+++ NaggerTests/NagTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NaggerTests
+{
+    [TestClass]
+    public class NagTests
+    {
+
+        [TestMethod]
+        public void CanActuallyTestItTest()
+        {
+            int b = 3;
+            Assert.IsTrue(b == 3);
+        }
+    
+    }
+}
