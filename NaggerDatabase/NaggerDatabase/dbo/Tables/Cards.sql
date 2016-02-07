428ef2e33a3e19ada4f687f18e1b64fc534d6dd4
GailBowen <gailbowen1@gmail.com>
2016-02-07 19:16:29 +0000

    Update database project

--- NaggerDatabase/NaggerDatabase/dbo/Tables/Cards.sql
+++ NaggerDatabase/NaggerDatabase/dbo/Tables/Cards.sql
@@ -2,7 +2,7 @@
     [ID]            INT            IDENTITY (1, 1) NOT NULL,
     [ColumnID]      INT            CONSTRAINT [DF_Cards_ColumnID] DEFAULT ((1)) NOT NULL,
     [BoardID]       INT            CONSTRAINT [DF_Cards_ListID] DEFAULT ((1)) NOT NULL,
-    [FrequencyID]      INT            NOT NULL,
+    [FrequencyID]   INT            NOT NULL,
     [Mandated]      BIT            CONSTRAINT [DF_Cards_Mandated] DEFAULT ((0)) NOT NULL,
     [LocationID]    INT            CONSTRAINT [DF_Cards_LocationID] DEFAULT ((1)) NOT NULL,
     [Title]         VARCHAR (50)   NOT NULL,
@@ -18,10 +18,12 @@
     [Archived]      BIT            CONSTRAINT [DF_Cards_Completed] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Cards] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_Cards_Boards] FOREIGN KEY ([BoardID]) REFERENCES [dbo].[Boards] ([ID]),
-    CONSTRAINT [FK_Cards_Frequencies] FOREIGN KEY ([FrequencyID]) REFERENCES [dbo].[Frequencies] ([ID]),
     CONSTRAINT [FK_Cards_Columns] FOREIGN KEY ([ColumnID]) REFERENCES [dbo].[Columns] ([ID]),
+    CONSTRAINT [FK_Cards_Frequencies] FOREIGN KEY ([FrequencyID]) REFERENCES [dbo].[Frequencies] ([DayCount]),
     CONSTRAINT [FK_Cards_Locations] FOREIGN KEY ([LocationID]) REFERENCES [dbo].[Locations] ([ID])
 );
+
+
 
 
 
--- NaggerDatabase/NaggerDatabase/dbo/Tables/Frequencies.sql
+++ NaggerDatabase/NaggerDatabase/dbo/Tables/Frequencies.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Frequencies] (
-    [ID]          INT          IDENTITY (1, 1) NOT NULL,
     [Description] VARCHAR (50) NOT NULL,
     [DayCount]    INT          CONSTRAINT [DF_Frequencies_DayCount] DEFAULT ((0)) NOT NULL,
-    CONSTRAINT [PK_Frequencies] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_Frequencies_1] PRIMARY KEY CLUSTERED ([DayCount] ASC)
 );
+
+
 
