b8dab3774640cc9637519581486e2c207bd1df9d
GailBowen <gailbowen1@gmail.com>
2016-03-07 14:50:33 +0000

    Update db project

---
+++ NaggerDatabase/NaggerDatabase/dbo/Stored Procedures/Cards_FetchCurrentAction.sql
@@ -0,0 +1,15 @@
+﻿-- [dbo].[Cards_FetchPenultimateAction] 2
+create PROCEDURE [dbo].[Cards_FetchCurrentAction]
+	@cardID int
+AS
+BEGIN
+	
+	SELECT a.*, c.*
+	FROM dbo.CardActions a
+	left join dbo.Cards c on c.id = a.CardID
+	where cardid = @cardID
+	ORDER BY a.id DESC
+	OFFSET 0 ROW
+	FETCH FIRST 1 ROW ONLY 
+	
+END
---
+++ NaggerDatabase/NaggerDatabase/dbo/Stored Procedures/Cards_FetchPenultimateAction.sql
@@ -0,0 +1,15 @@
+﻿-- [dbo].[Cards_FetchPenultimateAction] 2
+CREATE PROCEDURE [dbo].[Cards_FetchPenultimateAction]
+	@cardID int
+AS
+BEGIN
+	
+	SELECT a.*, c.*
+	FROM dbo.CardActions a
+	left join dbo.Cards c on c.id = a.CardID
+	where cardid = @cardID
+	ORDER BY a.id DESC
+	OFFSET 1 ROW
+	FETCH FIRST 1 ROW ONLY 
+	
+END
